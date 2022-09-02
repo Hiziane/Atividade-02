f7cdaf6564bf86a60f263a82829184e777c49e3d
Hiziane <91857224+Hiziane@users.noreply.github.com>
2022-09-02 18:46:23 -0300

    Add files via upload

--- src/main/java/br/com/tiacademy/nascimentos/controller/MatrizController.java
+++ src/main/java/br/com/tiacademy/nascimentos/controller/MatrizController.java
@@ -1,15 +1,34 @@
 package br.com.tiacademy.nascimentos.controller;
 
 import br.com.tiacademy.nascimentos.core.crud.CrudController;
+import br.com.tiacademy.nascimentos.domain.Bezerro;
 import br.com.tiacademy.nascimentos.domain.Matriz;
+import br.com.tiacademy.nascimentos.domain.proprietario;
+import br.com.tiacademy.nascimentos.dto.BezerroResumoDTO;
 import br.com.tiacademy.nascimentos.dto.MatrizDTO;
+import br.com.tiacademy.nascimentos.repository.BezerroRepository;
+import br.com.tiacademy.nascimentos.service.BezerroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/matriz")
-
 public class MatrizController extends CrudController<Matriz, MatrizDTO, Long> {
 
+    @Autowired
+    protected BezerroRepository bezerroRepository;
+    public BezerroRepository getBezerroRepository() {
+        return (BezerroRepository) this.bezerroRepository;
+    }
 
+    @GetMapping("/{id}/meus-bezerros")
+    public List<Bezerro> constultarBezerrosPorMatriz(@PathParam("id") Matriz id) {
+        var bezerros = this.getBezerroRepository().constultarBezerrosPorMatriz(id);
+        return bezerros;
+    }
 }
